b0457d092b20cb7feb268b91b66222425c1c4d60
NewView <40295938+ClnViewer@users.noreply.github.com>
2019-04-24 08:58:27 +0300

    Add files via upload

---
+++ CB-шаблон/Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := hello_world
+LOCAL_SRC_FILES := ./main.c
+LOCAL_C_INCLUDES := ./
+include $(BUILD_EXECUTABLE)
---
+++ CB-шаблон/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int a = 0;
+    int b = a;
+
+    printf("Hello world! (%d)\n", b);
+
+    return 0;
+}

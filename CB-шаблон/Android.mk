d6752f0239b8138e184ed69056cd36d157ea207b
NewView <40295938+ClnViewer@users.noreply.github.com>
2019-04-26 22:27:14 +0300

    Delete Android.mk

--- CB-шаблон/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := hello_world
-LOCAL_SRC_FILES := ./main.c
-LOCAL_C_INCLUDES := ./
-LOCAL_LDLIBS := -llog
-include $(BUILD_EXECUTABLE)

ca34f143e19d1029c531c5259816b4acb4ae9e2e
NewView <40295938+ClnViewer@users.noreply.github.com>
2019-04-26 10:41:28 +0300

    Add files via upload

---
+++ cbp2ndk/version.h
@@ -0,0 +1,30 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+	//Date Version Types
+	#define DATE "26"
+	#define MONTH "04"
+	#define YEAR "2019"
+	#define UBUNTU_VERSION_STYLE  "19.04"
+	
+	//Software Status
+	#define STATUS  ""
+	#define STATUS_SHORT  ""
+	
+	//Standard Version Type
+	#define MAJOR  0
+	#define MINOR  0
+	#define BUILD  2
+	#define REVISION  14
+	
+	//Miscellaneous Version Types
+	#define BUILDS_COUNT  2
+	#define RC_FILEVERSION 0,0,2,14
+	#define RC_FILEVERSION_STRING "0, 0, 2, 14\0"
+	#define FULLVERSION_STRING  "0.0.2.14"
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	#define BUILD_HISTORY  2
+	
+
+#endif //VERSION_H

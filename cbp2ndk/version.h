0fdcb32cb9927d673dddf9d52c85acb82b0380a2
NewView <40295938+ClnViewer@users.noreply.github.com>
2019-04-26 23:00:21 +0300

    Add files via upload

--- cbp2ndk/version.h
+++ cbp2ndk/version.h
@@ -1,30 +1,30 @@
-#ifndef VERSION_H
-#define VERSION_H
+#ifndef VERSION_LOCAL_H
+#define VERSION_LOCAL_H
 
 	//Date Version Types
-	#define DATE "26"
-	#define MONTH "04"
-	#define YEAR "2019"
-	#define UBUNTU_VERSION_STYLE  "19.04"
+	#define CBP_DATE "26"
+	#define CBP_MONTH "04"
+	#define CBP_YEAR "2019"
+	#define CBP_UBUNTU_VERSION_STYLE  "19.04"
 	
 	//Software Status
-	#define STATUS  ""
-	#define STATUS_SHORT  ""
+	#define CBP_STATUS  ""
+	#define CBP_STATUS_SHORT  ""
 	
 	//Standard Version Type
-	#define MAJOR  0
-	#define MINOR  0
-	#define BUILD  2
-	#define REVISION  14
+	#define CBP_MAJOR  0
+	#define CBP_MINOR  0
+	#define CBP_BUILD  15
+	#define CBP_REVISION  83
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT  2
-	#define RC_FILEVERSION 0,0,2,14
-	#define RC_FILEVERSION_STRING "0, 0, 2, 14\0"
-	#define FULLVERSION_STRING  "0.0.2.14"
+	#define CBP_BUILDS_COUNT  22
+	#define CBP_RC_FILEVERSION 0,0,15,83
+	#define CBP_RC_FILEVERSION_STRING "0, 0, 15, 83\0"
+	#define CBP_FULLVERSION_STRING  "0.0.15.83"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY  2
+	#define CBP_BUILD_HISTORY  15
 	
 
-#endif //VERSION_H
+#endif //VERSION_LOCAL_H

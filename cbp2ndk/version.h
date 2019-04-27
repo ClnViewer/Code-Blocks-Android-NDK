2f6cc34c9245dd8808cf0a9d5894f95d560f79f2
NewView <40295938+ClnViewer@users.noreply.github.com>
2019-04-27 16:05:29 +0300

    Add files via upload

--- cbp2ndk/version.h
+++ cbp2ndk/version.h
@@ -2,7 +2,7 @@
 #define VERSION_LOCAL_H
 
 	//Date Version Types
-	#define CBP_DATE "26"
+	#define CBP_DATE "27"
 	#define CBP_MONTH "04"
 	#define CBP_YEAR "2019"
 	#define CBP_UBUNTU_VERSION_STYLE  "19.04"
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define CBP_MAJOR  0
 	#define CBP_MINOR  0
-	#define CBP_BUILD  15
-	#define CBP_REVISION  83
+	#define CBP_BUILD  40
+	#define CBP_REVISION  214
 	
 	//Miscellaneous Version Types
-	#define CBP_BUILDS_COUNT  22
-	#define CBP_RC_FILEVERSION 0,0,15,83
-	#define CBP_RC_FILEVERSION_STRING "0, 0, 15, 83\0"
-	#define CBP_FULLVERSION_STRING  "0.0.15.83"
+	#define CBP_BUILDS_COUNT  70
+	#define CBP_RC_FILEVERSION 0,0,40,214
+	#define CBP_RC_FILEVERSION_STRING "0, 0, 40, 214\0"
+	#define CBP_FULLVERSION_STRING  "0.0.40.214"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define CBP_BUILD_HISTORY  15
+	#define CBP_BUILD_HISTORY  40
 	
 
 #endif //VERSION_LOCAL_H
